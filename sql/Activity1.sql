ea8e26376422e033a935290ce5595d5b3d0cbf1e
PrathyushaGIT <76985875+PrathyushaGIT@users.noreply.github.com>
2021-02-05 10:25:28 +0530

    Add files via upload

---
+++ sql/Activity1.sql
@@ -0,0 +1,7 @@
+REM   Script: Activity1
+REM   SDET_SQL_Activities
+
+Create TABLE salesman ( salesman_id int, salesman_name varchar2(20), salesman_city varchar2(20), 
+commission int);
+
+desc salesman;

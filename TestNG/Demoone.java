37be5f6c8978cf9583393191b5d6362222d2ba56
PrathyushaGIT <76985875+PrathyushaGIT@users.noreply.github.com>
2021-02-05 10:29:09 +0530

    Add files via upload

---
+++ TestNG/Demoone.java
@@ -0,0 +1,18 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoOne
+{
+
+	@Test
+	public void firstTestCase() 
+	{
+		System.out.println("This is the first test case in DemoOne");
+	}
+	@Test
+	public void secondTestCase()
+	{
+		System.out.println("This is the second test case in DemoOne");
+	}
+}
---
+++ TestNG/Demotwo.java
@@ -0,0 +1,12 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo 
+{
+  @Test
+  public void testCase() 
+  {
+	  System.out.println("This is the test case of DemoTwo");
+  }
+}

d5d7b4289ea7923f942646bfea6ad9b513d4d272
PrathyushaGIT <76985875+PrathyushaGIT@users.noreply.github.com>
2021-04-23 19:41:50 +0530

    Add files via upload

---
+++ BDD/LoginSteps.java
@@ -0,0 +1,56 @@
+package stepDefinitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    WebDriver driver;
+    WebDriverWait wait;
+    
+    @Given("^Login page$")
+    public void loginPage() {
+        //Setup instances
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/login-form");
+    }
+    
+    @When("^Type username and password$")
+    public void enterCredentials() {
+        //Enter username
+        driver.findElement(By.id("username")).sendKeys("admin");
+        //Enter password
+        driver.findElement(By.id("password")).sendKeys("password");
+        //Click Login
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+    
+    @Then("^page title and confirmation message$")
+    public void readTitleAndHeading() {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("action-confirmation")));
+        
+        //Read the page title and heading
+        String pageTitle = driver.getTitle();
+        String confirmMessage = driver.findElement(By.id("action-confirmation")).getText();
+        
+        //Print the page title and heading
+        System.out.println("Page title is: " + pageTitle);
+        System.out.println("Login message is: " + confirmMessage);
+    }
+    
+    @And("^Browser should be closed$")
+    public void cBrowser() {
+        //Close browser
+        driver.close();
+    }
+}
